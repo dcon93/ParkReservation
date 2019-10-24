0d38c3b435bfbd47b82a4269dc6a8a7a5db839a6
Marjorie Loresch <mloresch@gmail.com>
2019-10-24 11:42:57 -0400

    merged with master and imported our pojos into DAOs

--- module-2/09_Capstone/java/src/main/java/com/techelevator/dao/ReservationDAO.java
+++ module-2/09_Capstone/java/src/main/java/com/techelevator/dao/ReservationDAO.java
@@ -1,5 +1,9 @@
 package com.techelevator.dao;
 
+import java.util.ArrayList;
+
+import com.techelevator.Reservation;
+
 public interface ReservationDAO {
 	public boolean saveReservation(Reservation reservation); // returns true if the reservation was saved 
 	public boolean isReservationAvailable(Reservation reservation); // returns true if the reservation is available
--- module-2/09_Capstone/java/src/main/java/com/techelevator/dao/SiteDAO.java
+++ module-2/09_Capstone/java/src/main/java/com/techelevator/dao/SiteDAO.java
@@ -1,5 +1,9 @@
 package com.techelevator.dao;
 
+import java.util.ArrayList;
+
+import com.techelevator.Site;
+
 public interface SiteDAO {
 	public Site getSiteBySiteId(Long siteId);
 	public ArrayList<Site> getAllSitesByCampgroundId(Long campgroundId); // in order by site number
