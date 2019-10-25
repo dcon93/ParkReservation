1d3b02d62af30535764229b9af84340ce8722a8b
Marjorie Loresch <mloresch@gmail.com>
2019-10-25 15:01:54 -0400

    created CLI run method and Menu class outline

---
+++ module-2/09_Capstone/java/src/main/java/com/techelevator/Menu.java
@@ -0,0 +1,38 @@
+package com.techelevator;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Scanner;
+
+import javax.print.DocFlavor.INPUT_STREAM;
+import javax.sql.DataSource;
+
+import com.techelevator.dao.JDBCCampgroundDAO;
+import com.techelevator.dao.JDBCParkDAO;
+import com.techelevator.dao.JDBCReservationDAO;
+import com.techelevator.dao.JDBCSiteDAO;
+
+public class Menu {
+	JDBCParkDAO parkDAO;
+	JDBCSiteDAO siteDAO;
+	JDBCCampgroundDAO campgroundDAO;
+	JDBCReservationDAO reservationDAO;
+	Scanner userInput;
+	OutputStream out;
+	
+	public Menu(InputStream input, OutputStream output, DataSource datasource) {
+		userInput = new Scanner(input);
+		out = output;
+		parkDAO = new JDBCParkDAO(datasource);
+		siteDAO = new JDBCSiteDAO(datasource);
+		campgroundDAO = new JDBCCampgroundDAO(datasource);
+		reservationDAO = new JDBCReservationDAO(datasource);
+	}
+	
+	public Park selectAPark() {} // displays parks to user and returns their choice of park; returns null if they choose 'Q'
+	public int parkInfo(Park chosenPark) {} // Displays park info and menu options 1-3. Returns user's menu choice (1-3);
+	public boolean parkCampgrounds(Park chosenPark) {} // Displays  campgrounds for the given park; returns true if the user wants to make a reservation, false if they want to go to prev screen.
+	public void makeReservationByCampground(Park chosenPark) {} // Displays  campgrounds and lets user make reservation and stuff.
+	public void makeReservationByPark(Park chosenPark) {}// Displays  campgrounds and lets user make reservation and stuff.
+
+}
